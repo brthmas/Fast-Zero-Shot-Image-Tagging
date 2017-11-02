74a54757150490ba62883e49ade1ab27f9a32169
brthmas <32919251+brthmas@users.noreply.github.com>
2017-11-02 13:26:47 -0700

    Add files via upload

---
+++ fast0tag.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+#CNN to Classify Image
+from keras.applications.vgg19 import VGG19, preprocess_input
+from keras.preprocessing import image
+
+img_model = VGG19(include_top=False, pooling="max")
+
+img_path = ""
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+img_features = img_model.predict(x)
+
+#Ranking SVM to create word-association space
+import ranking
+
+X_train_words = [] #input tags as word vector
+y_train_words = [] #whether or not each tag is relevant to the image
+rank_svm = RankSVM().fit()
+word_features = rank_svm.predict(X_train_words)
+#We approximate the principle direction, w, with a linear regression model
+from sklearn.linear_model import LinearRegression
+
+w = LinearRegression().fit(img_features,word_features)
